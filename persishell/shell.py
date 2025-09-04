41b3f1fb5d0c231adf3d910836a83aaa94e42386
Jiakun Yan <jiakunyan1998@gmail.com>
2025-09-04 17:09:01 -0500

    fix tests

--- persishell/shell.py
+++ persishell/shell.py
@@ -1,3 +1,4 @@
+import re
 from subprocess import Popen, PIPE
 import os, sys
 import threading
@@ -14,6 +15,7 @@ class ThreadReadio(threading.Thread):
     def __init__(self, process, pipe, outfile, term_words, command):
         threading.Thread.__init__(self)
         self.ret = None
+        self.returncode = 0
         self.process = process
         self.pipe = pipe
         self.outfile = outfile
@@ -44,22 +46,35 @@ class ThreadReadio(threading.Thread):
 
 
 class PersiShell:
+    class CompletedProcess:
+        def __init__(self, args, returncode, stdout, stderr):
+            self.args = args
+            self.returncode = returncode
+            self.stdout = stdout
+            self.stderr = stderr
+
     def __init__(self):
         self.proc = Popen(['bash'], stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=False)
 
     # Run a command in the persistent shell
-    def run(self, command, to_print=True, timeout=None):
+    def run(self, command, print_output=True, timeout=None):
         if type(command) is list or type(command) is tuple:
             command = " ".join(command)
         secret = random.random()
-        term_words = "\n{} done\n".format(secret)
-        cmd = command + "; printf \"{}\"; >&2 printf \"{}\"\n".format(term_words, term_words)
+        term_words = f"\n__PERSISHELL_END__{secret}__\n"
+        # cmd = command + "; printf \"{}\"; >&2 printf \"{}\"\n".format(term_words, term_words)
+        # cmd = f"{command}; RETVAL=$?; printf \"{term_words}$RETVAL\"; >&2 printf \"{term_words}\"\n"
+        cmd = (
+            f"{command}; "
+            f"printf \"{term_words}\"; "
+            f">&2 printf \"{term_words}\"\n"
+        )
         print("PersiShell.run: " + command)
         sys.stdout.flush()
         self.proc.stdin.write(cmd.encode('UTF-8'))
         self.proc.stdin.flush()
 
-        if to_print:
+        if print_output:
             t1 = ThreadReadio(self.proc, self.proc.stdout, sys.stdout, term_words, command)
             t2 = ThreadReadio(self.proc, self.proc.stderr, sys.stderr, term_words, command)
         else:
@@ -79,13 +94,14 @@ class PersiShell:
         else:
             t1.join()
             t2.join()
-        return self.proc.poll(), t1.ret, t2.ret
+        
+        return self.CompletedProcess(command, t1.returncode, t1.ret, t2.ret)
     
     # Convenience functions
     def export(self, key, val):
-        errorcode, _, _ = self.run("export {}={}".format(key, val))
-        return errorcode
+        ret = self.run("export {}={}".format(key, val))
+        return ret.returncode
 
     def unset(self, key):
-        errorcode, _, _ = self.run("unset {}".format(key))
-        return errorcode
+        ret = self.run("unset {}".format(key))
+        return ret.returncode
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,9 +4,9 @@ def test_env():
     sh = PersiShell()
     errorcode = sh.export("TESTVAR", "VALUE")
     assert errorcode == 0
-    errorcode, stdout, stderr = sh.run("echo $TESTVAR")
-    assert stdout.strip() == "VALUE"
+    ret = sh.run("echo $TESTVAR")
+    assert ret.stdout.strip() == "VALUE"
     errorcode = sh.unset("TESTVAR")
     assert errorcode == 0
-    errorcode, stdout, stderr = sh.run("echo $TESTVAR")
-    assert stdout.strip() == ""
+    ret = sh.run("echo $TESTVAR")
+    assert ret.stdout.strip() == ""
